f8e6de25f8e5e7ca4f109fc1ab951ef2cc3c2895
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2025-01-16 18:40:44 +0100

    Fix spaces

--- src/JouveManager.Domain/TravelShipment.cs
+++ src/JouveManager.Domain/TravelShipment.cs
@@ -5,11 +5,9 @@ public class TravelShipment : Entity<Guid>
 {
     public Guid ShipmentId { get; set; }
     public Shipment Shipment { get; set; }
-
     public Guid TravelId { get; set; }
     public Travel Travel { get; set; }
-
     public bool Delivered { get; set; }
     public DateTime? DeliveryDate { get; set; }
-    public string FailureReason { get; set; } = string.Empty;
+    public string FailureReason { get; set; } = null;
 }
